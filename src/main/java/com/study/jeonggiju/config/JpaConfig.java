8c0ff638bc37c94c46b69ee89c1068873ea6d5dd
jeonggiju <halogiju123@gmail.com>
2025-12-21 21:44:18 +0900

    ci

--- src/main/java/com/study/jeonggiju/config/JpaConfig.java
+++ src/main/java/com/study/jeonggiju/config/JpaConfig.java
@@ -3,7 +3,6 @@ package com.study.jeonggiju.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// 자기전 휴대폰 X
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {
