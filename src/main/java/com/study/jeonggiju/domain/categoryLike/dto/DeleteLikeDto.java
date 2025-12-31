36f77551e127fe6552496412fa318ac678cf06f0
jeonggiju <halogiju123@gmail.com>
2026-01-01 00:41:23 +0900

    add: AddLikeDto.java and DeleteLikeDto.java 추가

---
+++ src/main/java/com/study/jeonggiju/domain/categoryLike/dto/DeleteLikeDto.java
@@ -0,0 +1,12 @@
+package com.study.jeonggiju.domain.categoryLike.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DeleteLikeDto {
+	UUID categoryId;
+}
