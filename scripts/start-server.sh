241eab8397a08150c11b57c7ac7906e10cddf8f9
jeonggiju <halogiju123@gmail.com>
2026-01-12 04:26:46 +0900

    workflow test38

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -7,7 +7,6 @@ docker pull 624545858643.dkr.ecr.ap-northeast-2.amazonaws.com/jeonggiju/life:lat
 docker pull 624545858643.dkr.ecr.ap-northeast-2.amazonaws.com/jeonggiju/nginx:latest
 
 cd /home/ubuntu/jeonggiju-life
-docker-compose -f docker-compose.prod.yml down --remove-orphans 2>/dev/null || true
-docker-compose -f docker-compose.prod.yml pull
-docker-compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml down --remove-orphans 2>/dev/null || true
+docker compose -f docker-compose.prod.yml up -d --build
 echo "--------------- 서버 배포 끝 ------------------"
