2b3c3a92304d423a243b1cb436ea837a12308c73
jeonggiju <halogiju123@gmail.com>
2026-01-12 03:52:34 +0900

    workflow test32-컴포즈 추가

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,9 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop jeonggiju-life-server || true
 docker rm jeonggiju-life-server || true
-docker pull 624545858643.dkr.ecr.ap-northeast-2.amazonaws.com/jeonggiju/life:latest
-docker run -d --name jeonggiju-life-server -p 8080:8080 624545858643.dkr.ecr.ap-northeast-2.amazonaws.com/jeonggiju/life:latest
+
+docker-compose -f docker-compose.prod.yml down --remove-orphans 2>/dev/null || true
+docker-compose -f docker-compose.prod.yml pull
+docker-compose -f docker-compose.prod.yml up -d
+
 echo "--------------- 서버 배포 끝 ------------------"
