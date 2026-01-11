b39a16725d11849abc3581fa2e008260df00af7b
jeonggiju <halogiju123@gmail.com>
2026-01-12 04:02:16 +0900

    workflow test34

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,7 +3,8 @@
 echo "--------------- 서버 배포 시작 -----------------"
 docker stop jeonggiju-life-server || true
 docker rm jeonggiju-life-server || true
-
+ls
+pwd
 docker-compose -f docker-compose.prod.yml down --remove-orphans 2>/dev/null || true
 docker-compose -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.prod.yml up -d
