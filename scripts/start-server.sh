760615d9084c03ca9e9737e015573a49eec675d4
jeonggiju <halogiju123@gmail.com>
2026-01-12 02:05:50 +0900

    workflow test20

---
+++ scripts/start-server.sh
@@ -0,0 +1,6 @@
+echo "--------------- 서버 배포 시작 -----------------"
+docker stop jeonggiju-life-server || true
+docker rm jeonggiju-life-server || true
+docker pull 624545858643.dkr.ecr.ap-northeast-2.amazonaws.com/jeonggiju/life:latest
+docker run -d --name instagram-server -p 8080:8080 624545858643.dkr.ecr.ap-northeast-2.amazonaws.com/jeonggiju/life:latest
+echo "--------------- 서버 배포 끝 -----------------"
