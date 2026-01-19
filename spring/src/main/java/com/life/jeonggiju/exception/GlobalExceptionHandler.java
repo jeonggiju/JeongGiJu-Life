875d5690343d0aafaf8de67e779f70c745636f7d
jeonggiju <halogiju123@gmailc.om>
2026-01-19 16:10:20 +0900

    add: GlobalExceptionHandler에 ErrorResponse를 활용하도록 추가

--- spring/src/main/java/com/life/jeonggiju/exception/GlobalExceptionHandler.java
+++ spring/src/main/java/com/life/jeonggiju/exception/GlobalExceptionHandler.java
@@ -16,9 +16,17 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(BaseException.class)
+	public ResponseEntity<ErrorResponse> handleBaseException(BaseException ex) {
+		ErrorResponse response = new ErrorResponse(ex);
+		return ResponseEntity
+				.status(ex.getErrorCode().getHttpStatus())
+				.body(response);
+	}
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> handleValidationExceptions(
-		MethodArgumentNotValidException ex) {
+	public ResponseEntity<ErrorResponse> handleValidationExceptions(
+			MethodArgumentNotValidException ex) {
 
 		Map<String, String> errors = new HashMap<>();
 		ex.getBindingResult().getAllErrors().forEach((error) -> {
@@ -27,27 +35,21 @@ public class GlobalExceptionHandler {
 			errors.put(fieldName, errorMessage);
 		});
 
-		return ResponseEntity.badRequest().body(Map.of(
-			"success", false,
-			"message", "입력값 검증 실패",
-			"errors", errors
-		));
+		ErrorResponse response = new ErrorResponse(ex, HttpStatus.BAD_REQUEST.value());
+		return ResponseEntity.badRequest().body(response);
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<Map<String, Object>> handleAllExceptions(
-		Exception ex,
-		HttpServletRequest request
+	public ResponseEntity<ErrorResponse> handleAllExceptions(
+			Exception ex,
+			HttpServletRequest request
 	) {
-		String contentType = request.getHeader("Accept");
-		if (contentType != null && contentType.contains("text/event-stream")) {
+		if (request.getHeader("Accept") != null &&
+				request.getHeader("Accept").contains("text/event-stream")) {
 			return null;
 		}
 
-		Map<String, Object> errorDetails = new HashMap<>();
-		errorDetails.put("message", ex.getMessage());
-		errorDetails.put("timestamp", LocalDateTime.now());
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-			.body(errorDetails);
+		ErrorResponse response = new ErrorResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 	}
 }
