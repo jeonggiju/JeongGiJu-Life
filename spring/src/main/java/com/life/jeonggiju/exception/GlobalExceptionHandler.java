93a8010d9fdde61a3d412cd889a2ef8dc46183be
jeonggiju <halogiju123@gmailc.om>
2026-01-19 16:16:55 +0900

    refactor: 공통 유저 에러 예외 처리

--- spring/src/main/java/com/life/jeonggiju/exception/GlobalExceptionHandler.java
+++ spring/src/main/java/com/life/jeonggiju/exception/GlobalExceptionHandler.java
@@ -1,41 +1,32 @@
 package com.life.jeonggiju.exception;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.life.jeonggiju.domain.user.exception.UserNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(BaseException.class)
-	public ResponseEntity<ErrorResponse> handleBaseException(BaseException ex) {
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleUserException(UserNotFoundException ex){
 		ErrorResponse response = new ErrorResponse(ex);
 		return ResponseEntity
 			.status(ex.getErrorCode().getHttpStatus())
 			.body(response);
 	}
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorResponse> handleValidationExceptions(
-		MethodArgumentNotValidException ex) {
-
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError)error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-
-		ErrorResponse response = new ErrorResponse(ex, HttpStatus.BAD_REQUEST.value());
-		return ResponseEntity.badRequest().body(response);
+	@ExceptionHandler(BaseException.class)
+	public ResponseEntity<ErrorResponse> handleBaseException(BaseException ex) {
+		ErrorResponse response = new ErrorResponse(ex);
+		return ResponseEntity
+				.status(ex.getErrorCode().getHttpStatus())
+				.body(response);
 	}
 
 	@ExceptionHandler(Exception.class)
