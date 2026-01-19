3a019ac20cad51a68f0a96ba08e523c04c20ec6c
jeonggiju <halogiju123@gmailc.om>
2026-01-19 15:38:20 +0900

    add: MDC 로깅을 위한 config 추가

--- spring/src/main/java/com/life/jeonggiju/config/WebConfig.java
+++ spring/src/main/java/com/life/jeonggiju/config/WebConfig.java
@@ -1,14 +1,22 @@
 package com.life.jeonggiju.config;
 
+import com.life.jeonggiju.log.MDCLoggingInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAsync
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry){
+		registry.addInterceptor(new MDCLoggingInterceptor())
+				.addPathPatterns("/**");
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
