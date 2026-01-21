539571051b746d6d7070266a1df88a375dd5cb8d
jeonggiju <halogiju123@gmailc.om>
2026-01-21 11:30:24 +0900

    refactor: GlobalExceptionHandler, UserException 제거

--- spring/src/main/java/com/life/jeonggiju/exception/GlobalExceptionHandler.java
+++ spring/src/main/java/com/life/jeonggiju/exception/GlobalExceptionHandler.java
@@ -1,35 +1,21 @@
 package com.life.jeonggiju.exception;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.life.jeonggiju.domain.user.exception.UserException;
-import com.life.jeonggiju.domain.user.exception.UserNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
-
-	@ExceptionHandler(UserException.class)
-	public ResponseEntity<ErrorResponse> handleUserException(UserNotFoundException ex){
-		ErrorResponse response = new ErrorResponse(ex);
-		return ResponseEntity
-			.status(ex.getErrorCode().getHttpStatus())
-			.body(response);
-	}
-
 	@ExceptionHandler(BaseException.class)
 	public ResponseEntity<ErrorResponse> handleBaseException(BaseException ex) {
 		ErrorResponse response = new ErrorResponse(ex);
 		return ResponseEntity
-				.status(ex.getErrorCode().getHttpStatus())
-				.body(response);
+			.status(ex.getErrorCode().getHttpStatus())
+			.body(response);
 	}
 
 	@ExceptionHandler(Exception.class)
