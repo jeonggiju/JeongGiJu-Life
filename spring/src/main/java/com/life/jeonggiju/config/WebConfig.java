5686d560c14860c5084b25ed158a919b54706077
jeonggiju <halogiju123@gmailc.om>
2026-02-08 20:11:20 +0900

    fix: CorsConfigurationSource 빈을 직접 주입

--- spring/src/main/java/com/life/jeonggiju/config/WebConfig.java
+++ spring/src/main/java/com/life/jeonggiju/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.life.jeonggiju.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,17 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new MDCLoggingInterceptor())
 			.addPathPatterns("/**");
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOriginPatterns("*")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-			.allowedHeaders("*")
-			.exposedHeaders("*")
-			.allowCredentials(true)
-			.maxAge(3600);
 	}
 
 	@Override
